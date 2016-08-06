5bb2bcf2d0f9ab4613d694eeaf3497859551abd8
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2016-08-06 20:07:30 +0200

    Creating methods for altering items in DataManagement components.

--- components/base/DataManagement.jsx
+++ components/base/DataManagement.jsx
@@ -1,22 +1,131 @@
 import React from 'react';
-import {objectValues} from '../../lib/helpers.js'
+import {objectValues, cloneObject} from '../../lib/helpers.js'
+
 
 export class DataManagementList extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            items: [],
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
+
+    handleChange(attribute, value, index) {
+        let items = this.state.items;
+        let data = {};
+        data[attribute] = value;
+        items[index] = Object.assign(cloneObject(items[index]), data);
+
+        this.setState({items});
+    }
+
+    deleteItem(index) {
+        if (index >= this.state.items.length) {
+            console.log('Invalid index value');
+            return;
+        }
+
+        let items = this.state.items.filter((item, i) =>  index !== i);
+
+        this.setState({items});
+    }
+
+    addItem() {
+        let items = this.state.items;
+        let new_item = {};
+
+        for(let i = 0; i < this.props.columns.length; i++) {
+            let column = this.props.columns[i];
+
+            switch (column.inputType) {
+                case 'checkbox':
+                    new_item[column.dataAttributeName] = false;
+                    break;
+                default:
+                    new_item[column.dataAttributeName] = "";
+            }
+        }
+
+        items.push(new_item);
+
+        this.setState({items});
+    }
+
+    componentDidMount() {
+        this.setState({
+            items: [
+                {
+                    "first_name": "Daniel",
+                    "id": 2,
+                    "is_admin": false,
+                    "is_contractor": true,
+                    "is_creator": true,
+                    "last_name": "Zawłocki",
+                    "login": "danzaw"
+                },
+                {
+                    "first_name": "Przemek",
+                    "id": 3,
+                    "is_admin": true,
+                    "is_contractor": true,
+                    "is_creator": false,
+                    "last_name": "Ociepa",
+                    "login": "przoci"
+                },
+                {
+                    "first_name": "Daniel",
+                    "id": 1,
+                    "is_admin": true,
+                    "is_contractor": true,
+                    "is_creator": true,
+                    "last_name": "Zawłocki",
+                    "login": "admin"
+                }
+            ]
+        });
     }
 
     render() {
-        let headerCols = this.props.headerCols.map(function(item, i) {
-            return (<th key={i}>{item}</th>);
+        let columns = this.props.columns;
+        let headerCols = columns.map((item, i) => {
+            return (<th key={i+1}>{item.headerColumnName}</th>);
+        });
+
+        let handleChange = this.handleChange;
+        let deleteItem = this.deleteItem;
+
+        headerCols.unshift(<th key={0}>#</th>);
+        headerCols.push(<th key={headerCols.length}>Edytuj</th>);
+        headerCols.push(<th key={headerCols.length}>Usuń</th>);
+
+        let items = this.state.items.map(function(item, i) {
+            return (<DataManagementItem
+                        key={i} index={i} data={item}
+                        columns={columns} handleChange={handleChange}
+                        deleteItem={deleteItem.bind(this, i)}
+                    />)
         });
 
         return (
-            <table id={this.props.htmlId} className="table table-striped">
+            <table id={this.props.htmlId} className="table table-striped data-management-cmp">
                 <thead>
                     <tr>{headerCols}</tr>
                 </thead>
-                <tbody></tbody>
+                <tbody>
+                    {items}
+                    <tr>
+                        <td colSpan={headerCols.length}>
+                            <button onClick={this.addItem} className="btn btn-warning">
+                                <span className="glyphicon glyphicon-plus"></span> Dodaj
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         );
     }
@@ -25,16 +134,124 @@ export class DataManagementList extends React.Component {
 export class DataManagementItem extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            editing: false,
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.toggleEdit = this.toggleEdit.bind(this);
+        this._prepareColumns = this._prepareColumns.bind(this);
+        this._prepareHtmlInput = this._prepareHtmlInput.bind(this);
     }
 
     render() {
-        let values = objectValues(this.props.data);
-        let items = values.map((item, i) => <td key={i}>{item}</td>);
+        let items = this._prepareColumns();
 
         return (
             <tr>
                 {items}
             </tr>
         );
+    }
+
+    isEditing() {
+        return this.state.editing;
+    }
+
+    handleChange(event) {
+        let attribute = event.target.name;
+        let value = event.target.value;
+        let index = this.props.index;
+
+        if (event.target.type === 'checkbox') {
+            value = !this.props.data[attribute];
+        }
+
+        this.props.handleChange(attribute, value, index);
+    }
+
+    toggleEdit() {
+        this.setState({ editing: !this.state.editing });
+    }
+
+    _prepareColumns() {
+        let data = this.props.data;
+        let items = [];
+
+        items.push(<td key={items.length}>{this.props.index + 1}</td>);
+
+        this.props.columns.forEach((item, i) => {
+            if (!this.isEditing()) {
+                let html = this._prepareHtml(
+                    item.inputType,
+                    data[item.dataAttributeName],
+                    items.length
+                );
+
+                items.push(html);
+            } else {
+                let htmlInput = this._prepareHtmlInput(
+                    item.inputType,
+                    data[item.dataAttributeName],
+                    item.dataAttributeName,
+                    this.handleChange
+                );
+
+                items.push(
+                    <td key={items.length}>
+                        {htmlInput}
+                    </td>
+                );
+            }
+        });
+
+        if (!this.isEditing()) {
+            items.push(
+                <td key={items.length}>
+                    <button onClick={this.toggleEdit} className="btn btn-info">
+                        <span className="glyphicon glyphicon-edit"></span> Edytuj
+                    </button>
+                </td>
+            );
+        } else {
+            items.push(
+                <td key={items.length}>
+                    <button onClick={this.toggleEdit} className="btn btn-success">
+                        <span className="glyphicon glyphicon-save"></span> Zapisz
+                    </button>
+                </td>
+            );
+        }
+
+        items.push(
+            <td key={items.length}>
+                <button onClick={this.props.deleteItem} className="btn btn-danger">
+                    <span className="glyphicon glyphicon-remove"></span> Usuń
+                </button>
+            </td>
+        );
+
+        return items;
+    }
+
+    _prepareHtmlInput(type, value, name, onChange) {
+        switch (type) {
+            case 'checkbox':
+                return (<input type={type} name={name} checked={value} onChange={onChange}/>);
+                break;
+            default:
+                return (<input type={type} name={name} value={value} onChange={onChange}/>);
+        }
+    }
+
+    _prepareHtml(type, value, key) {
+        switch (type) {
+            case 'checkbox':
+                return (<td key={key}>{value ? 'tak' : 'nie'}</td>);
+                break;
+            default:
+                return (<td key={key}>{value}</td>);
+        }
     }
 }
--- components/base/DataManagement_test.jsx
+++ components/base/DataManagement_test.jsx
@@ -4,12 +4,49 @@ import {DataManagementList, DataManagementItem} from './DataManagement.jsx';
 import {objectValues} from '../../lib/helpers.js'
 
 describe('DataManagementList', function() {
-    it("should render table with given id and header columns", function() {
-        let htmlId = 'my-list';
-        let headerColsNames = ['id', 'name'];
-        let list = ReactTestUtils.renderIntoDocument(
-            <DataManagementList htmlId={htmlId} headerCols={headerColsNames}/>
+    let htmlId, columns, list, mockItems;
+
+    beforeEach(function() {
+        htmlId = 'my-list';
+
+        columns = [
+            { headerColumnName: 'id', dataAttributeName: 'id', inputType: 'number'},
+            { headerColumnName: 'name', dataAttributeName: 'name', inputType: 'text'},
+            { headerColumnName: 'admin', dataAttributeName: 'admin', inputType: 'checkbox'},
+        ];
+
+        list = ReactTestUtils.renderIntoDocument(
+            <DataManagementList htmlId={htmlId} columns={columns}/>
         );
+
+        mockItems = [
+            {
+                "first_name": "Daniel",
+                "id": 2,
+                "is_admin": false,
+                "is_contractor": true,
+                "is_creator": true,
+                "last_name": "Zawłocki",
+                "login": "danzaw"
+            },
+            {
+                "first_name": "Przemek",
+                "id": 3,
+                "is_admin": true,
+                "is_contractor": true,
+                "is_creator": false,
+                "last_name": "Ociepa",
+                "login": "przoci"
+            },
+        ];
+
+        list.setState({
+            items: mockItems
+        });
+    });
+
+    it("should render table with given id and header columns", function() {
+        expect(list.state.items.length).toBe(mockItems.length);
 
         let table = ReactTestUtils.findRenderedDOMComponentWithTag(
             list, 'table'
@@ -21,22 +58,75 @@ describe('DataManagementList', function() {
             list, 'th'
         );
 
-        expect(ths.length).toBe(2);
+        expect(ths[0].textContent).toBe("#");
+        expect(ths[columns.length + 1].textContent).toBe("Edytuj");
+        expect(ths[columns.length + 2].textContent).toBe("Usuń");
+
+        expect(ths.length).toBe(columns.length + 3);
+    });
+
+    it('should be able to alter the items', function() {
+        list.handleChange('first_name', 'Konrad', 0);
+
+        expect(list.state.items[0].first_name).toBe('Konrad');
+
+        list.handleChange('is_admin', true, 0);
+        expect(list.state.items[0].is_admin).toBe(true);
+
+        list.handleChange('is_admin', false, 0);
+        expect(list.state.items[0].is_admin).toBe(false);
+    });
+
+    it('should be able to delete the item', function() {
+        expect(list.state.items.length).toBe(mockItems.length);
+
+        let item = list.state.items[1];
+        list.deleteItem(1);
+
+        expect(list.state.items.length).toBe(mockItems.length - 1);
+        expect(list.state.items).not.toContain(item);
+    });
+
+    it('should be able to add new item', function() {
+
+        expect(list.state.items.length).toBe(2);
+        list.addItem();
+        expect(list.state.items.length).toBe(3);
+
+        let item = list.state.items.pop();
+
+        expect(item).toEqual({
+            id: "",
+            name: "",
+            admin: false
+        });
     });
 });
 
 describe('DataManagementItem', function() {
-    it('should render td element', function() {
-        let mockData = {
+    let mockData, columns, index, item;
+
+    beforeEach(function() {
+        mockData = {
             id: 1,
             name: 'Daniel',
-            login: 'admin'
+            admin: true
         };
 
-        let item = ReactTestUtils.renderIntoDocument(
-            <DataManagementItem data={mockData} />
-        );
+        columns = [
+            { headerColumnName: 'id', dataAttributeName: 'id', inputType: 'number'},
+            { headerColumnName: 'name', dataAttributeName: 'name', inputType: 'text'},
+            { headerColumnName: 'admin', dataAttributeName: 'admin', inputType: 'checkbox'},
+        ];
 
+        index = 3;
+
+        item = ReactTestUtils.renderIntoDocument(
+            <DataManagementItem index={index} data={mockData} columns={columns}/>
+        );
+    });
+
+    it('should render td element', function() {
         let tr = ReactTestUtils.scryRenderedDOMComponentsWithTag(
             item, 'tr'
         );
@@ -47,12 +137,28 @@ describe('DataManagementItem', function() {
             item, 'td'
         );
 
-        expect(tds.length).toBe(3);
-
         let values = objectValues(mockData);
 
-        for(let i = 0; i < values.length; i++) {
-            expect(tds[i].textContent).toBe(values[i].toString());
+        expect(tds.length).toBe(columns.length + 3);
+        expect(tds[0].textContent).toBe((index + 1).toString());
+
+        for(let i = 0; i < columns.length; i++) {
+            if (columns[i].inputType == 'checkbox') {
+                if (mockData[columns[i].dataAttributeName]) {
+                    expect(tds[i + 1].textContent).toBe('tak');
+                } else {
+                    expect(tds[i + 1].textContent).toBe('nie');
+                }
+            } else {
+                expect(tds[i + 1].textContent).toBe(mockData[columns[i].dataAttributeName].toString());
+            }
         }
+    });
+
+    it('should switch between "edit" end "show" mode', function() {
+        let is_editing = item.state.editing;
+        item.toggleEdit();
+
+        expect(item.state.editing).not.toBe(is_editing);
     });
 });
